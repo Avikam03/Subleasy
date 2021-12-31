b600aca86e159ef23ed55ba5ed8bd9cac7cf9d8e
Avikam Mangla <avikam03@gmail.com>
2021-12-30 22:40:51 -0500

    removed comments. same working condition as last commit

--- lib/routes/Dashboard.dart
+++ lib/routes/Dashboard.dart
@@ -1,93 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-
-// bool nullCheck(object){
-//   if(object == null){
-//     return false;
-//   }
-//   else{
-//     return true;
-//   }
-// }
-
-// class Dashboard extends StatefulWidget {
-//   Dashboard({Key? key}) : super(key: key);
-
-//   @override
-//   _DashboardState createState() => _DashboardState();
-// }
-
- 
-
-// class _DashboardState extends State<Dashboard> {
-//   String name = 'ok';
-
-//   @override
-//   void initState(){
-//     super.initState();
-
-//     FirebaseFirestore.instance 
-//     .collection('users')
-//     .doc(FirebaseAuth.instance.currentUser?.uid)
-//     .get()
-//     .then((DocumentSnapshot documentSnapshot) {
-//       setState(() {
-//         name = (documentSnapshot.data()! as Map)['name'];
-//       });
-      
-//     });
-//   }  
-  
-//   @override
-//   Widget build(BuildContext context) {
-
-//   print(name);
-       
-//     return Material(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           const Text('Dashboard',
-//           style: TextStyle(fontSize: 30),
-//           ),
-//           Text(name),
-
-//           RaisedButton(
-//             child:
-//              const Text(
-//               "My Listings",
-//               style: TextStyle(
-//                 fontSize: 22,
-//                 color: Color(0xff000000)
-//               ),
-//             ),
-//             onPressed: (){
-//               Navigator.pushReplacementNamed(context, '/mylistings');
-//             }
-//           ),
-
-
-//           RaisedButton(
-//             onPressed: (){
-//               SignOut();
-//             }
-//           )
-//         ]
-//       )
-//     );
-//   }
-
-//   void SignOut() async{
-//     await FirebaseAuth.instance.signOut();
-//     Navigator.pushReplacementNamed(context, '/');
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -203,7 +113,7 @@ class MyAppState extends State<MyApp> {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              backgroundColor: Color(0xFF426BFF),
+              backgroundColor: const Color(0xFF426BFF),
               title:const Text('Dashboard'),
             ),
             body: SingleChildScrollView(
